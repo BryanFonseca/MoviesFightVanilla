14f61fd2d5157b8d7e24653a43ab22db43f424e4
Isaac Fonseca <isaac1805@hotmail.com>
2021-08-05 00:16:19 -0500

    start search widget and debouncing it

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hi there!');
+//API key: fd6fe8b7
+let request = async (searchTerm) =>
+{
+  let config = 
+    {
+      params: 
+      {
+	apikey: 'fd6fe8b7',
+	s: searchTerm 
+      }
+    };
+  let response = await axios.get('http://www.omdbapi.com/', config);
+  console.log(response.data);
+}
+
+const input = document.querySelector('#text-input');
+let timeoutId;
+const onInput = (event) => 
+{
+  if(timeoutId)
+  {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {request(event.target.value)}, 1000);
+};
+input.addEventListener('input', onInput);
+
