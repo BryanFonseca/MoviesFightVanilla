0f3066ded0c5648ad7bdb63eac93501f7bb5e930
Isaac Fonseca <isaac1805@hotmail.com>
2021-08-07 23:23:18 -0500

    Complete dropdown menu

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let request = async (searchTerm) =>
 	s: searchTerm 
       }
     };
+  //posiblemente añadir try-catch para eliminar el error en GET
   let response = await axios.get('http://www.omdbapi.com/', config);
   if(response.data.Error)
   {
@@ -16,20 +17,55 @@ let request = async (searchTerm) =>
   }
   return response.data.Search;
 }
-const input = document.querySelector('#text-input');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search for a movie</b></label>
+<input class="input" type="text">
+<div class="dropdown">
+	<div class="dropdown-menu">
+	  <div class="dropdown-content results">
+	  </div>
+	</div>
+      </div>
+      <div id="target"></div>
+`
+;
+const input = document.querySelector('.input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 const onInput = async (event) => 
 {
   const movies = await request(event.target.value);
+
+  if(!movies.length)
+  {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
+  resultsWrapper.innerHTML = '';
+  dropdown.classList.add('is-active');
   if(movies)
   {
     for(let movie of movies)
     {
-      const div = document.createElement('div');
-      div.innerHTML = `
-      <img src="${movie.Poster}"/>
-      <h1>${movie.Title}</h1>
-    `;
-      document.querySelector('#target').appendChild(div);
+      const option = document.createElement('a');
+
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+      option.classList.add('dropdown-item');
+      option.innerHTML = `
+      <img src="${imgSrc}"/>
+      ${movie.Title}
+      `;
+      resultsWrapper.appendChild(option);
+
+      option.addEventListener('click', () => {
+	dropdown.classList.remove('is-active');
+	input.value = movie.Title;
+	OnMovieSelect(movie);
+      })
     }
   }
 };
@@ -40,3 +76,24 @@ const onInput = async (event) =>
 //considerar que la linea de abajo solo se ejecuta de manera síncrona, el callback devuelto por debounce
 //se ejecuta de manera asíncrona al gatillarse el evento.
 input.addEventListener('input', debounce(onInput, 500));
+input.addEventListener('click', onInput)
+
+document.addEventListener('click', (event) => {
+  if(!root.contains(event.target))
+  {
+    dropdown.classList.remove('is-active');
+  }
+}) 
+
+const OnMovieSelect = async movie =>
+{
+  const config = 
+    {
+      params: {
+	apikey: 'fd6fe8b7',
+	i: movie.imdbID
+      }
+    }
+  let response = await axios.get('http://www.omdbapi.com/', config)
+  console.log(response);
+}
