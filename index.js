591f075b96c28b4e3cc1d5dbc73e8c37cb0e4261
Isaac Fonseca <isaac1805@hotmail.com>
2021-08-07 23:36:41 -0500

    Show statistics

--- index.js
+++ index.js
@@ -95,5 +95,46 @@ const OnMovieSelect = async movie =>
       }
     }
   let response = await axios.get('http://www.omdbapi.com/', config)
-  console.log(response);
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+}
+
+const movieTemplate = (movieDetail) =>
+{
+  const content = `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+	<img src="${movieDetail.Poster}" alt="">
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+	<h1>${movieDetail.Title}</h1>
+	<h4>${movieDetail.Genre}</h1>
+	<p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IDMB Rating</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+  `;
+  return content;
 }
